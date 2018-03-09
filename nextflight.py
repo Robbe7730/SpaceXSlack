f060646b825f9a9548fdd44044f092d6496e4794
Robbe Van Herck <robbevanherck1@gmail.com>
2018-03-09 17:05:05 +0100

    channeled shite

--- nextflight.py
+++ nextflight.py
@@ -7,11 +7,11 @@ def nextflight():
     print(request.values)
     print(request.data)
     if request.method == 'POST' and request.values and request.values["text"]:
-        return nextflightoffset(request.values["text"])
+        return inchannel(nextflightoffset(request.values["text"]))
     ret = ""
     for line in flight_info.get_launch():
         ret += line + "\n"
-    return ret
+    return inchannel(ret)
 
 @app.route("/nextflight/<offset>", methods = ['GET', 'POST'])
 def nextflightoffset(offset):
@@ -28,6 +28,9 @@ def nextflightoffset(offset):
     for line in flight_info.get_launch(int(offset)):
         ret += line + "\n"
     return ret
+
+def inchannel(text):
+    return '{"response_type": "in_channel","text":"' + text + '"}'
 
 if __name__ == "__main__":
     app.run()
