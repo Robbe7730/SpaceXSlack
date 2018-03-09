91af862739734812271da80b2b4cfdb5b1e63fda
Robbe Van Herck <robbevanherck1@gmail.com>
2018-03-09 16:42:38 +0100

    REVERTED MORE SHITE THAT DOES NOT WORK

--- nextflight.py
+++ nextflight.py
@@ -9,7 +9,7 @@ def nextflight():
         return nextflightoffset(request.form["text"])
     ret = ""
     for line in flight_info.get_launch():
-        ret += line + "<br>" if request.method == "GET" else "\n"
+        ret += line + "\n"
     return ret
 
 @app.route("/nextflight/<offset>", methods = ['GET', 'POST'])
@@ -19,13 +19,13 @@ def nextflightoffset(offset):
         if letter in "0123456789":
             hasnumbers = True
         elif letter != "-":
-            return f"Invalid character '{letter}' in offset, offset can only contain numbers or a '-'" + ("<br>" if request.method == "GET" else "\n")
+            return f"Invalid character '{letter}' in offset, offset can only contain numbers or a '-'\n"
     if not hasnumbers:
-        return "Invalid offset, offset must be a number" + ("<br>" if request.method == "GET" else "\n")
+        return "Invalid offset, offset must be a number\n"
 
     ret = ""
     for line in flight_info.get_launch(int(offset)):
-        ret += line + ("<br>" if request.method == "GET" else "\n")
+        ret += line + "\n"
     return ret
 
 if __name__ == "__main__":
