d76f206f5a044268a680abf98719928ffcf20b75
Robbe Van Herck <robbevanherck1@gmail.com>
2018-03-09 17:14:58 +0100

    reverted shite

--- nextflight.py
+++ nextflight.py
@@ -4,14 +4,12 @@ app = Flask(__name__)
 
 @app.route("/nextflight", methods = ['GET', 'POST'])
 def nextflight():
-    print(request.values)
-    print(request.data)
     if request.method == 'POST' and request.values and request.values["text"]:
-        return inchannel(nextflightoffset(request.values["text"]))
+        return nextflightoffset(request.values["text"])
     ret = ""
     for line in flight_info.get_launch():
         ret += line + "\n"
-    return inchannel(ret)
+    return ret
 
 @app.route("/nextflight/<offset>", methods = ['GET', 'POST'])
 def nextflightoffset(offset):
@@ -28,9 +26,6 @@ def nextflightoffset(offset):
     for line in flight_info.get_launch(int(offset)):
         ret += line + "\n"
     return ret
-
-def inchannel(text):
-    return '{"response_type": "in_channel","text":"' + text + '"}'
 
 if __name__ == "__main__":
     app.run()
